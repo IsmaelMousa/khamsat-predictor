df2bb3a2e24a3e2495f73a1db1c43ba11e561890
Ismael Mousa <ismaelramzimousa@gmail.com>
2024-12-10 22:20:44 +0200

    prepare the data receving

--- routers/offer.py
+++ routers/offer.py
@@ -1,16 +1,21 @@
 import pandas as pd
 from fastapi import APIRouter
 
-from mappers import to_numeric
+from mappers import to_numeric, encode, get_keys
 from models import Request
 
 router = APIRouter(prefix="/offer", tags=["Offer"])
-
-
-@router.post(path="/", summary="Enter The Offer Features")
-def predict(request: Request):
+@router.post(path="/", summary="Enter the offer features to predict the price.")
+def enter(request: Request):
     data = request.model_dump()
-    data = to_numeric(pd.DataFrame([data]), columns=["duration", "offer_response_time", "owner_response_time"])
-    data = data.to_dict(orient="records")
 
-    return data
+    data = to_numeric(pd.DataFrame([data]), columns=["duration", "offer_response_time", "owner_response_time", "owner_level"])
+
+    category_encoded = pd.DataFrame(data["category_name"].apply(lambda value: encode(value=value, column="category_name")).tolist(), columns=[key for key in get_keys(column="Category Name")])
+    service_encoded = pd.DataFrame(data["service_name"].apply(lambda value: encode(value=value, column="service_name")).tolist(), columns=[key for key in get_keys(column="Service Name")])
+
+    data = data.drop(columns=["category_name", "service_name"])
+
+    data = pd.concat(objs=[data, category_encoded, service_encoded], axis=1)
+
+    return data.to_dict(orient="records")[0]
