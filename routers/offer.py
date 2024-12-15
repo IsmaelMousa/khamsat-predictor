db3411fec49ec42528eeccb3a5dbc594ee9c7fa6
Ismael Mousa <ismaelramzimousa@gmail.com>
2024-12-15 23:47:59 +0200

    job job som3a, just write docs string

--- routers/offer.py
+++ routers/offer.py
@@ -1,21 +1,42 @@
+import os
+
+import joblib
 import pandas as pd
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
-from mappers import to_numeric, encode, get_keys
-from models import Request
+from mappers import to_numeric, encode, get_keys, get_names
+from models import Request, Response
 
+root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "experiments"))
+model_path = os.path.join(root, "Random Forest Classifier", "0", "balanced", "artifacts", "model", "model.pkl")
+
+model = joblib.load(model_path)
+feature_names = get_names()
+database = {}
 router = APIRouter(prefix="/offer", tags=["Offer"])
-@router.post(path="/", summary="Enter the offer features to predict the price.")
+
+
+@router.post(path="/", summary="Enter the offer features.")
 def enter(request: Request):
     data = request.model_dump()
-
     data = to_numeric(pd.DataFrame([data]), columns=["duration", "offer_response_time", "owner_response_time", "owner_level"])
 
     category_encoded = pd.DataFrame(data["category_name"].apply(lambda value: encode(value=value, column="category_name")).tolist(), columns=[key for key in get_keys(column="Category Name")])
     service_encoded = pd.DataFrame(data["service_name"].apply(lambda value: encode(value=value, column="service_name")).tolist(), columns=[key for key in get_keys(column="Service Name")])
 
     data = data.drop(columns=["category_name", "service_name"])
-
     data = pd.concat(objs=[data, category_encoded, service_encoded], axis=1)
+    data = data.rename(columns=feature_names)
 
-    return data.to_dict(orient="records")[0]
+    database[0] = data.to_dict(orient="records")[0]
+
+    return Response(status=status.HTTP_201_CREATED, message="Successfully entered offer information.").model_dump()
+
+@router.get(path="/{offer_id}", summary="Predict the price.")
+def get_offer(offer_id: int):
+    if offer_id in database:
+        data = pd.DataFrame([database[offer_id]])
+
+        return Response(status=status.HTTP_200_OK, message=f"${model.predict(data).tolist()[0]}0").model_dump()
+
+    return Response(status=status.HTTP_404_NOT_FOUND, message=f"No data found with ID {offer_id}.").model_dump()
