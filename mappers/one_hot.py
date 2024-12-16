23fe9073d057591abeb1a22cf8ff41ee52200ebb
Ismael Mousa <ismaelramzimousa@gmail.com>
2024-12-16 22:00:50 +0200

    write the docs string and type hints

--- mappers/one_hot.py
+++ mappers/one_hot.py
@@ -8,9 +8,10 @@ root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "mappers", 
 
 def get_keys(column: str) -> list[str]:
     """
-    TODO
-    :param column:
-    :return:
+    Retrieves the keys (categorical values) from the mapping JSON file for a given column.
+
+    :param column: the name of the column to fetch keys for
+    :return: a list of keys from the mapping file
     """
     file_name = column.lower().replace(" ", "_") + ".json"
     file_path = os.path.join(root, file_name)
@@ -18,15 +19,17 @@ def get_keys(column: str) -> list[str]:
     with open(file_path, "r", encoding="utf-8") as file:
         mapping = load(file)
 
-    return mapping.keys()
+    return list(mapping.keys())
 
 
 def encode(value: str, column: str) -> np.ndarray:
     """
-    TODO
-    :param value:
-    :param column:
-    :return:
+    Encodes a given value for a specified column into a one-hot vector,
+    where the values set to 1 and the other set to 0.
+
+    :param value: the value to encode
+    :param column: the name of the column associated with the value
+    :return: a one-hot encoded NumPy array representing the value
     """
     file_name = column.lower().replace(" ", "_") + ".json"
     file_path = os.path.join(root, file_name)
