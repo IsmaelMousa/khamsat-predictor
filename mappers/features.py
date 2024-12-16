ae8318b4daf103d949dc3269d719bb1c4b33077d
Ismael Mousa <ismaelramzimousa@gmail.com>
2024-12-16 22:00:36 +0200

    write the docs string and type hints

--- mappers/features.py
+++ mappers/features.py
@@ -4,10 +4,12 @@ from json import load
 root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "mappers", "to_categorical"))
 path = os.path.join(root, "feature_names.json")
 
-def get_names():
+
+def get_names() -> dict[str, str]:
     """
-    TODO
-    :return:
+    Loads and returns the feature name mappings from a JSON file.
+
+    :return: a dictionary containing feature name mappings
     """
     with open(path, "r", encoding="utf-8") as file:
         mapping = load(file)
